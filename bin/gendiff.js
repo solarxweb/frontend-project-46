a2092348110128cb754758491aa39bd70d6970e4
dev <legendaryje@gmail.com>
2024-05-29 19:40:24 +0300

    option --format added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+const fs = require('fs');
 const program = new Command;
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0');
+
+program
+  .option('-f, --format [type]', 'output format')
  
 program.parse();
