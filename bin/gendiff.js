7506af0587396eb561f5304506ff5137e8a764f7
dev <legendaryje@gmail.com>
2024-05-29 19:25:00 +0300

    gendiff/new command

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command;
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+ 
+program.parse();
