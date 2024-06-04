54e8c2d9d087505447771d12c32720e14853a2fc
dev <legendaryje@gmail.com>
2024-06-04 22:45:22 +0300

    refactoring

--- src/createTree.js
+++ src/createTree.js
@@ -1,26 +1,28 @@
 import _ from 'lodash';
 
-const getDiff = (data1, data2, el) => {
-  let diff = {};
-  if (_.isObject(data1[el]) && _.isObject(data2[el])) {
-    diff = { state: 'nested', value: createTree(data1[el], data2[el]) };
-  } else if (data1[el] === data2[el]) {
-    diff = { state: 'unchanged', value:data1[el] };
-  } else if (!_.has(data1, el)) {
-    diff = { state: 'added', value: data2[el] };
-  } else if (!_.has(data2, el)) {
-    diff = { state: 'deleted', value: data1[el] };
-  } else if (data1[el] && data2[el] && data1[el] !== data2[el]) {
-    diff = { state: 'changed', oldValue: data1[el], newValue: data2[el] };
-  }
-  return diff;
+const prefix = {
+  added: '+ ',
+  unchanged: '  ',
+  deleted: '- ',
 };
 
 const createTree = (data1, data2) => {
   const commonKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-  const compare = commonKeys.reduce((acc, el) => {
-    acc[el] = getDiff(data1, data2, el);
-    return acc;
+  const compare = commonKeys.map((el) => {
+    switch (true) {
+      case _.isObject(data1[el]) && _.isObject(data2[el]):
+        return { [el]:createTree(data1[el], data2[el]), state:'nested'};
+      case data1[el] === data2[el]:
+        return { [el]:data1[el], state: 'unchanged',};
+      case !_.has(data1, el):
+        return { [el]:data2[el], state: 'added'};
+      case !_.has(data2, el):
+        return { [el]:data1[el], state: 'deleted' };
+      case data1[el] && data2[el] && data1[el] !== data2[el]:
+        return { [el]: { oldValue: data1[el], newValue: data2[el]}, state: 'changed'};
+      default:
+        throw new Error('Unexpected situation')
+    }
   }, {});
   return compare;
 };
