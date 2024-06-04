a1c9203d1e81a97f04b1c8cf41bb62e1cd414d03
dev <legendaryje@gmail.com>
2024-06-04 21:36:27 +0300

    refactoring

--- src/createTree.js
+++ src/createTree.js
@@ -1,24 +1,19 @@
 import _ from 'lodash';
 
 const getDiff = (data1, data2, el) => {
+  let diff = {};
   if (_.isObject(data1[el]) && _.isObject(data2[el])) {
-    return { [el]:createTree(data1[el], data2[el]), state: 'nested'} ;
+    diff = { state: 'nested', value: createTree(data1[el], data2[el]) };
+  } else if (data1[el] === data2[el]) {
+    diff = { state: 'unchanged', value:data1[el] };
+  } else if (!_.has(data1, el)) {
+    diff = { state: 'added', value: data2[el] };
+  } else if (!_.has(data2, el)) {
+    diff = { state: 'deleted', value: data1[el] };
+  } else if (data1[el] && data2[el] && data1[el] !== data2[el]) {
+    diff = { state: 'changed', oldValue: data1[el], newValue: data2[el] };
   }
-  if (data1[el] === data2[el]) {
-    return { [el]:data1[el], state: 'unchanged' };
-  }
-  if (!_.has(data1, el)) {
-    return { [el]:data2[el], state: 'added'};
-  }
-  if (!_.has(data2, el)) {
-    return { [el]:data1[el], state: 'deleted' };
-  }
-  if (data1[el] && data2[el]) {
-    if (data1[el] !== data2[el]) {
-      return { [el]:el, oldValue: data1[el], newValue: data2[el], state: 'changed'};
-    }
-  }
-  return {};
+  return diff;
 };
 
 const createTree = (data1, data2) => {
