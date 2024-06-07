99aaa21d51cce09748479d6f269813e8ba04e40d
dev <legendaryje@gmail.com>
2024-06-07 19:26:06 +0300

    update gendiff

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@
 import path from 'node:path';
 import fs from 'node:fs';
 import parse from './parser.js';
-import process from 'node:process';
-import createTree from '../src/createTree.js';
+import createTree from './compareObjs.js';
+import outputFormat from './formatter/outputFormat.js'
 
 const getPath = (pathTo) => path.resolve(process.cwd(), pathTo);
 const getExtension = (fileName) => path.extname(fileName).slice(1);
 const getData = (file) => parse(fs.readFileSync(file, 'utf-8'), getExtension(file));
 
-const gendiff = (firstPath, secondPath) => {
+const gendiff = (firstPath, secondPath, format = 'stylish') => {
   const fileFromPath1 = getPath(firstPath);
   const fileFromPath2 = getPath(secondPath);
   const data1= getData(fileFromPath1);
   const data2 = getData(fileFromPath2);
   const tree = createTree(data1, data2);
-  return tree;
+  return outputFormat(tree, format);
 };
 
 export default gendiff;
