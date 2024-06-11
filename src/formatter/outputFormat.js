c339c2aa288af0e1ee42d1a387405dda1fb0cf68
dev <legendaryje@gmail.com>
2024-06-11 22:31:26 +0300

    rename

--- src/formatter/outputFormat.js
+++
@@ -1,8 +0,0 @@
-import stylishView from './stylish.js';
-
-const outputFormat = {
-  stylish: stylishView,
-  json: JSON.stringify,
-};
-
-export default (data, format) => outputFormat[format](data);
