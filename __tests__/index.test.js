9fd2560d5617adf71c68bc87d416c22bcc2bb785
dev <legendaryje@gmail.com>
2024-06-11 15:30:16 +0300

    rename test file

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs'
+import { expect, test } from '@jest/globals';
+import gendiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+
+test('output json result in "stylish" format', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedResult.txt'));
+});
+
+test('output yaml result in "stylish" format', () => {
+  const file1 = getFixturePath('file1.yaml');
+  const file2 = getFixturePath('file2.yaml');
+  expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedResult.txt'));
+});
