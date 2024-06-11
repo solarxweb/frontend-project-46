22e251fbc3fc5b7564c3dc2b6a6066546292d787
dev <legendaryje@gmail.com>
2024-06-11 22:33:14 +0300

    add new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-import { expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,14 +10,33 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-test('output json result in "stylish" format', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
-  expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
-});
+let file1;
+let file2;
 
-test('output yaml result in "stylish" format', () => {
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.yaml');
-  expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+describe('tests each format/view for output', () => {
+  test('output json result in "stylish" format', () => {
+    file1 = getFixturePath('file1.json');
+    file2 = getFixturePath('file2.json');
+    expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+  });
+  test('output json result in "stylish" format', () => {
+    file1 = getFixturePath('file1.json');
+    file2 = getFixturePath('file2.json');
+    expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+  });
+  test('output yaml result in "stylish" format', () => {
+    file1 = getFixturePath('file1.yaml');
+    file2 = getFixturePath('file2.yaml');
+    expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+  });
+  test('check output when format is plain', () => {
+    file1 = getFixturePath('file1.yaml');
+    file2 = getFixturePath('file2.yaml');
+    expect(gendiff(file1, file2, 'plain')).toEqual(readFile('expectedPlain.txt'));
+  });
+  test('check output when format is "json"', () => {
+    file1 = getFixturePath('file1.json');
+    file2 = getFixturePath('file2.json');
+    expect(gendiff(file1, file2, 'json')).toEqual(readFile('expectedJSON.txt'));
+  });
 });
