38cb519c7d4a10180edb42e49d98aa320e2261c3
dev <legendaryje@gmail.com>
2024-06-12 14:46:26 +0300

    fix "lets" in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,28 +10,21 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 
-let file1;
-let file2;
-
 describe('tests each format/view for output', () => {
+  const json1 = getFixturePath('file1.json');
+  const json2 = getFixturePath('file2.json');
+  const yaml1 = getFixturePath('file1.yml');
+  const yaml2 = getFixturePath('file2.yml');
   test('output json result in "stylish" format', () => {
-    file1 = getFixturePath('file1.json');
-    file2 = getFixturePath('file2.json');
-    expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+    expect(gendiff(json1, json2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
   });
   test('output yaml result in "stylish" format', () => {
-    file1 = getFixturePath('file1.yml');
-    file2 = getFixturePath('file2.yml');
-    expect(gendiff(file1, file2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
+    expect(gendiff(yaml1, yaml2, 'stylish')).toEqual(readFile('expectedStylish.txt'));
   });
   test('check output when format is plain', () => {
-    file1 = getFixturePath('file1.yml');
-    file2 = getFixturePath('file2.yml');
-    expect(gendiff(file1, file2, 'plain')).toEqual(readFile('expectedPlain.txt'));
+    expect(gendiff(json1, json2, 'plain')).toEqual(readFile('expectedPlain.txt'));
   });
   test('check output when format is "json"', () => {
-    file1 = getFixturePath('file1.json');
-    file2 = getFixturePath('file2.json');
-    expect(gendiff(file1, file2, 'json')).toEqual(readFile('expectedJSON.txt'));
+    expect(gendiff(yaml1, yaml2, 'json')).toEqual(readFile('expectedJSON.txt'));
   });
 });
